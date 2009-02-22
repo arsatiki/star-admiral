29d3cfbe2fc7e7df3ee67266614be101a433b71b
Antti Rasinen <ars@iki.fi>
2009-02-22 20:19:41 +0200

    Added splash-screen

--- urls.py
+++ urls.py
@@ -3,12 +3,17 @@ from django.conf import settings
 
 from django.contrib import admin
 
+from django.views.generic import simple, list_detail, create_update
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-                       (r'^static/(?P<path>.*)$',
-                         'django.views.static.serve', 
-                         {'document_root': settings.MEDIA_ROOT}),
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', 
+        {'document_root': settings.MEDIA_ROOT}),
+
+    # Splash screen
+    ('^$', '')
+                        
 
                        (r'^admin/(.*)', admin.site.root),
                       )
