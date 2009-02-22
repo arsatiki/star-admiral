8b2f702e48e3505a4484fad14a39f01f4089d6ed
Antti Rasinen <ars@iki.fi>
2009-02-22 18:42:02 +0200

    fleshed out the game-models

--- games/models.py
+++ games/models.py
@@ -1,9 +1,20 @@
 from django.db import models
+from django.contrib.auth import models as auth
 
 
-def Game(models.Model):
+class Game(models.Model):
+    name = models.CharField()
+    owner = models.ForeignKey(auth.User)
+
+class Turn(models.Model):
+    number = models.PositiveSmallIntegerField()
+    game = models.ForeignKey(Game)
+
+    class Meta:
+        unique_together = ('number', 'game')
+
+class Faction(models.Models):
+    game = models.ForeignKey(Game)
+    user = models.ForeignKey(auth.User)
     name = models.CharField()
     
-
-def Faction(models.Models):
-    user = models.ForeignKey(auth.User)
