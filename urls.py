0f5119c5a04e1e4d4a532e71f9f47b442f2279a3
Antti Rasinen <ars@iki.fi>
2009-03-02 08:13:58 +0200

    Added logout. (NB! No login :)

--- urls.py
+++ urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import *
 from django.conf import settings
 
 from django.contrib import admin
+from django.contrib.auth import views as auth
 
 from django.views.generic import simple
 
@@ -13,6 +14,10 @@ urlpatterns = patterns('',
     
     # Splash screen
     ('^$', simple.direct_to_template, {'template': 'index.html'}),
+    
+    # Login and logout TODO
+    url('^login/', auth.login, {}, name='login'),
+    url('^logout/', auth.logout, {'next_page': '/games/'}, name='logout'),
     
     # Games
     ('^games/', include('games.urls')),
