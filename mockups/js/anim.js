eefd08700c941cbef6fd52e86ac4d3768805acca
Antti Rasinen <ars@iki.fi>
2009-08-25 07:52:54 +0300

    canvasmanager implementation

--- mockups/js/anim.js
+++ mockups/js/anim.js
@@ -1,26 +1,61 @@
-var ARMADATA = ARMADATA || {};
 
-ARMADATA.canvasManager = function() {
-    var context;
-    
-    var show_bg = false, paused = true;
-        
+var sceneObjectSort = SortBy('z', 'created');
+
+var canvasManager = (function() {
+
     return {
+        'time': 0,
+        
         fromElement: function(el) {
-            context = el.getContext('2d');
+            this.context = el.getContext('2d');
             return context;
         },
         
-        loop: function() {
-            if (paused)
-                return;
+        update: function(t_delta) {
+            var k, current;
+            var by_time, by_self;
+            this.time += t_delta;
             
-            if (show_bg) {
-                // do shit
+            for (k = 0; k < this.nodes.length; k++) {
+                current = this.nodes[k];
+                current.node.update(t_delta);
+                
+                by_time = current.dies && this.time > current.dies;
+                current.node.dead |= by_time;
             }
+            
+            this.nodes = this.nodes.filter(function(c) {
+                return !c.node.dead;
+            });
+            
+            return this;
+        }, 
+        
+        draw: function() {
+            this.nodes.sort(sceneObjectSort);
+            this.nodes.forEach(function(x) {x.draw(this.ctx);});
+        }, 
+        
+        addNode: function(node, z, TTL) {
+            if (!z || z < 0) { z = 0; }
+
+            var new_node = {
+                'node': node,
+                'created': this.time,
+                'z': z
+            };
+            
+            if (TTL) { new_node.dies = new_node.created + TTL; }
+
+            this.nodes.push(new_node);
+            
+            return new_node;
+        }
+        
             
             
             
         }
     };
-} ();
+}) ();
+
